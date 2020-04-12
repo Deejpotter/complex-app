140040a78744b1ceb844f59e67481fe58a0d0244
Daniel <deejpotter@gmail.com>
2020-04-12 15:34:29 +1000

    Added gravitar functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 exports.login = function (req, res) {
     let user = new User(req.body)
     user.login().then(function(result) {
-        req.session.user = {favColor: "blue", username: user.data.username}
+        req.session.user = {avatar: user.avatar, username: user.data.username}
         req.session.save(function() {
             res.redirect('/')
         })
@@ -23,18 +23,26 @@ exports.logout = function (req, res) {
 
 exports.register = function (req, res) {
     let user = new User(req.body)
-    user.register()
-    if (user.errors.length) {
-        res.send(user.errors)
-    } else {
-        res.send('Congratulations')
-    }
+    user.register().then(() => {
+        req.session.user = {username: user.data.username, avatar: user.avatar}
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    }).catch((regErrors) => {
+        regErrors.forEach(function(error) {
+            req.flash('regErrors', error)
+        })
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    })
+    
 }
 
 exports.home = function (req, res) {
     if (req.session.user) {
-        res.render('home-dashboard', {username: req.session.user.username})
+        res.render('home-dashboard', {username: req.session.user.username, avatar: req.session.user.avatar})
     } else {
-        res.render('home-guest', {errors: req.flash('errors')})
+        res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
     }
 }
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const validator = require('validator')
 const usersCollection = require('../db').db().collection('users')
+const md5 = require('md5')
 
 let User = function (data) {
     this.data = data
@@ -20,15 +21,30 @@ User.prototype.cleanUp = function() {
     }
 }
 
-User.prototype.validate = function () {
-    if (this.data.username == "") {this.errors.push("You must provide a username")}
-    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push('Usernames can only contain letters and numbers')}
-    if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide an email address")}
-    if (this.data.password == "") {this.errors.push("You must provide a password")}
-    if (this.data.password.length > 0 && this.data.password.length < 6) {this.errors.push("Password must be at least 6 characters")}
-    if (this.data.password.length > 50) {this.errors.push("Password can not exceed 50 characters")}
-    if (this.data.password.length > 0 && this.data.password.length < 3) {this.errors.push("Username must be at least 3 characters")}
-    if (this.data.password.length > 30) {this.errors.push("Username can not exceed 30 characters")}
+User.prototype.validate = function() {
+    return new Promise(async (resolve, reject) => {
+        if (this.data.username == "") {this.errors.push("You must provide a username")}
+        if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push('Usernames can only contain letters and numbers')}
+        if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide an email address")}
+        if (this.data.password == "") {this.errors.push("You must provide a password")}
+        if (this.data.password.length > 0 && this.data.password.length < 6) {this.errors.push("Password must be at least 6 characters")}
+        if (this.data.password.length > 50) {this.errors.push("Password can not exceed 50 characters")}
+        if (this.data.password.length > 0 && this.data.password.length < 3) {this.errors.push("Username must be at least 3 characters")}
+        if (this.data.password.length > 30) {this.errors.push("Username can not exceed 30 characters")}
+    
+        // Only if username is valid then check to see if it is taken
+        if (this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
+            let usernameExists = await usersCollection.findOne({username: this.data.username})
+            if(usernameExists) {this.errors.push('That username is already taken')}
+        }
+    
+        // Only if email is valid then check to see if it is taken
+        if (validator.isEmail(this.data.email)) {
+            let emailExists = await usersCollection.findOne({email: this.data.emaill})
+            if(emailExists) {this.errors.push('That email is already taken')}
+        }
+        resolve()
+    })
 }
 
 User.prototype.login = function () {
@@ -36,6 +52,8 @@ User.prototype.login = function () {
         this.cleanUp()
         usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
             if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+                this.data = attemptedUser
+                this.getAvatar()
                 resolve('Congrats')
             } else {
                 reject('Invalid username or password')
@@ -46,19 +64,29 @@ User.prototype.login = function () {
     })
 }
 
-User.prototype.register = function () {
-    // Step 1 - Validate user data
-    this.cleanUp()
-    this.validate()
+User.prototype.register = function() {
+    return new Promise(async (resolve, reject) => {
+        // Step 1 - Validate user data
+        this.cleanUp()
+        await this.validate()
+    
+        // Step 2 - Only if there are no validation errors
+        // then save the user data into the database
+        if (!this.errors.length) {
+            // Hash user password
+            let salt = bcrypt.genSaltSync(10)
+            this.data.password = bcrypt.hashSync(this.data.password, salt)
+            usersCollection.insertOne(this.data)
+            this.getAvatar()
+            resolve()
+        } else {
+            reject(this.errors)
+        }
+    })
+}
 
-    // Step 2 - Only if there are no validation errors
-    // then save the user data into the database
-    if (!this.errors.length) {
-        // Hash user password
-        let salt = bcrypt.genSaltSync(10)
-        this.data.password = bcrypt.hashSync(this.data.password, salt)
-        usersCollection.insertOne(this.data)
-    }
+User.prototype.getAvatar = function() {
+    this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
 }
 
 module.exports = User
